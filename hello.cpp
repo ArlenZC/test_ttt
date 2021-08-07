77b370662261d90a002bb7fe6f0e8250a8fed0e0
ArlenZC <z_ccumt@163.com>
2021-08-07 22:58:06 +0800

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ArlenZC on 2021/5/29.
+//
+#include "hello.h"
+
+void hello()
+{
+    cout << "Hello, World!" << endl;
+    cout << "how can you study hard"<<endl;
+}
---
+++ hello.h
@@ -0,0 +1,13 @@
+//
+// Created by ArlenZC on 2021/5/29.
+//
+
+#ifndef TEST_HELLO_H
+#define TEST_HELLO_H
+#include <iostream>
+using namespace  std;
+
+void hello();
+
+
+#endif //TEST_HELLO_H
